978653813045f85c28f6fcc1752959374d57b2e2
Mihir Vir <80036612+MihirVir@users.noreply.github.com>
2024-01-17 16:27:02 +0530

    making container component more generic

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import { SplitScreen } from "./components/LayoutPattern/split-screen";
-
+import axios from "axios";
 import { CurrentUserLoader } from "./components/ContainerComponents/current-user-loader";
 import { UserInfo } from "./components/ContainerComponents/user-info";
 import { LargeAuthorListItem } from "./components/LayoutPattern/Authors/LargeListItems";
@@ -14,6 +14,7 @@ import { books } from "./data/books";
 import { UserLoader } from "./components/ContainerComponents/user-loader";
 import { ResourceLoader } from "./components/ContainerComponents/resource-loader";
 import { BookInfo } from "./components/ContainerComponents/book-info";
+import { DataSource } from "./components/ContainerComponents/data-source";
 // const LeftSideComponent = ({title}) => {
 //   return (
 //     <h2 style = {{backgroundColor: "crimson"}}>
@@ -39,15 +40,27 @@ import { BookInfo } from "./components/ContainerComponents/book-info";
 // <Modal>
 //   <LargeBookListItem  book = {books[0]} />
 // </Modal>
+
+const getDataFromServer = async (url) => {
+  const response = await axios.get(url)
+  return response.data;
+}
 function App() {
+  /*
+  The ResourceLoader is more flexible than any other component loader we build rn 
+  its because we can fetch UserInfo and BookInfo from only One Loader 
+  */
+ {/* <ResourceLoader resourceUrl={"http://localhost:9090/user/2"} resourceName={"user"}>
+   <UserInfo />
+ </ResourceLoader>
+ <ResourceLoader resourceUrl={"http://localhost:9090/books/1"} resourceName={"book"}>
+   <BookInfo />
+ </ResourceLoader> */}
   return (
     <>
-      <ResourceLoader resourceUrl={"http://localhost:9090/user/2"} resourceName={"user"}>
+      <DataSource getData={() => getDataFromServer("http://localhost:9090/user/2")} resourceName={"user"}>
         <UserInfo />
-      </ResourceLoader>
-      <ResourceLoader resourceUrl={"http://localhost:9090/books/1"} resourceName={"book"}>
-        <BookInfo />
-      </ResourceLoader>
+      </DataSource>
     </>
   )
 }
